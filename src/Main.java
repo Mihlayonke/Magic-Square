2a86e3a5987ac7619eae1940873fafe3f14930a3
Mihlayonke Tembe <mihlatembe@outlook.com>
2022-07-30 20:27:45 +0200

    Update and rename MainApp.java to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MainApp {
+public class Main {
 
     public static void main(String[] args) {
 
@@ -9,24 +9,24 @@ public class MainApp {
         System.out.print("Enter an odd number for a length: ");
         int length = scanner.nextInt();
         
-        //Rotate a square by (0x0°, 1x90°, 2x90°, 3x90°)...
-        System.out.print("Enter direction of a square, (0=0°, 1=90°, 2=180°, 3=270°): ");
-        int rotate = scanner.nextInt();
+        //Rotate a square clockwise by (0x0°, 1x90°, 2x90°, 3x90°)...
+        System.out.print("Enter rotation of a square, (0=0°, 1=90°, 2=180°, 3=270°): ");
+        int rotation = scanner.nextInt();
 
-        if ((length % 2 != 0) && (rotate >= 0 && rotate <= 3)){
-            //Square with length x length dimension...
-            int [][] square = new int [length][length];
+        if ((length % 2 != 0) && (rotation >= 0 && rotation <= 3)){
+            //Square with length x length dimension of numbers...
+            int [][] numbers = new int [length][length];
 
             //Declare and create an object...
-            MagicSquare magicSquare = new MagicSquare(length, square, rotate);
+            Square square = new Square(length, numbers, rotation);
             System.out.println();
             System.out.println("___ "+ length +" x "+ length +" Magic Square ___");
             System.out.println();
 
             //Generate a magic square than is (n x n) and rotate in degrees...
-            magicSquare.generateSquare(length, rotate);
+            square.generateSquare(length);
             System.out.println();
-            System.out.println("The sum of each row, column and diagonal is "+magicSquare.sum);
+            System.out.println("The sum of each row, column and diagonal is "+square.sum);
             System.out.println("___ END ___");
 
         }else{
